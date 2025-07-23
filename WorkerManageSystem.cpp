4b4e6d347eaf15b7ce28f99e537fc9e35f12cc5c
Yiming Pu <pymmmmmm@qq.com>
2025-07-23 09:11:26 +0800

    完成菜单功能

--- WorkerManageSystem.cpp
+++ WorkerManageSystem.cpp
@@ -5,8 +5,36 @@ int main() {
     // 实例化管理者对象
     WorkerManager wm;
 
-    // 调用展示菜单成员函数
-    wm.Show_Menu();
+    int choice = 0; // 用来存储用户的选项
+
+    while (true) {
+        // 调用展示菜单成员函数
+        wm.Show_Menu();
+
+        std::cout << "请选择你的操作：" << std::endl;
+        std::cin >> choice;;
+
+        switch (choice) {
+            case 0:
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
+            default:
+                break;
+        }
+    }
 
     std::cout << "Press Enter to continue...";
     std::cin.get();
